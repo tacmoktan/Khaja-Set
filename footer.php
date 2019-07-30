29bdb1b4458b42ac792840b289e8f704c4051460
iamtashi <tacmoktan@gmail.com>
2019-07-30 18:25:26 +0545

    update header, footer, styles

--- footer.php
+++ footer.php
@@ -1,7 +1,12 @@
     <!--FOOTER OPEN-->
-    <div class="container-fluid">
-        <div class="jumbotron">
+    <div class="container-fluid" id="footer">
+        <div id="header">
+            <a  href="index.php"> <img id="logo" src="image/logo.png"></a>
+            <a  id="logo-text" href="index.php"> Khaja Set</a>    
+        </div>
+        <div>
            <div class="row" id="footer-row">
+               
                <div class="col-sm-5">
                 <h4>CONTACT</h4>
                 <dl>
@@ -22,17 +27,20 @@
                     <dd><a href="instagram.com"><i class="fa fa-instagram" style="font-size: 20px;color:#2C2C2C;"></i></a></dd>
                     <dt>Youtube</dt>
                     <dd><a href="youtube.com"><i class="fa fa-youtube" style="font-size: 20px;color:#2C2C2C;"></i></a></dd>
-                </dl>
-        
+                </dl>  
             </div> 
+
         </div> 
 
+        <hr style="border-color:#e8e5e5;"/>
+
+        <div id="copyright-panel">
+            All rights reserved &copy; <b style="color: #fb2828;"><u>Khaja Set</u></b> 2017.
+        </div>
     </div>
-    <div class="panel panel-default">
-        <p>   All rights reserved &copy; <b style="color: #fb2828;"><u>Khaja Set</u></b> 2017.</p> 
-    </div>
-    </div>
-    <!--FOOTER CLOSE-->
+    
+</div>
+<!--FOOTER CLOSE-->
 </body>
 
 </html>
--- header.php
+++ header.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width,initial-scale=1">
-   <!-- Latest compiled and minified CSS -->
+    <!-- Latest compiled and minified CSS -->
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
 
     <!-- jQuery library -->
@@ -22,34 +22,35 @@
 </head>
 
 <body>
-<!--NAV OPEN-->
+    <!--NAV OPEN-->
     <nav class="navbar navbar-default">
-           <div class="container-fluid" >
-                <div class="row">
-                    <div class="col-sm-2">
-                    <div class="navbar-header"> 
-                        <a class="navbar-brand" href="index.php"> <img id="logo" src="image/logo.png"></a>
-                        <a class="navbar-brand" id="logo-text" href="index.php" style="color: #fb2828;"> Khaja Set</a>    
-                        <button type="button" class=" navbar-toggle glyphicon glyphicon-menu-hamburger" data-toggle="collapse" data-target="#navi"></button>
-                                
+     <div class="container-fluid">
+        <div class="row">
+            <div class="col-sm-2">
+                <div class="navbar-header"> 
+                    <div id="header">
+                        <a  href="index.php"> <img id="logo" src="image/logo.png"></a>
+                        <a  id="logo-text" href="index.php"> Khaja Set</a>    
                     </div>
-                    </div>
-                    <div class="col-sm-10">
-                     <div class="collapse navbar-collapse" id="navi">   
-                        <ul class="nav navbar-nav">
-                        <li><a href="index.php">Home</a></li>
-                        <li><a href="menu.php">Menu</a></li>
-                        <li><a href="order.php">Order</a></li>
-                        <li><a href="about.php">About</a></li>
-                        
-                        </ul>
-                        <ul class="nav navbar-nav navbar-right">
-                            <li><a href="register.php"><span class="glyphicon glyphicon-user"> </span> Sign Up</a></li>
-                            <li><a href="login.php"><span class="glyphicon glyphicon-log-in"></span> Login</a></li>
-                        </ul>    
-                    </div>
-                    </div>
+                    <button type="button" class=" navbar-toggle glyphicon glyphicon-menu-hamburger" data-toggle="collapse" data-target="#navi"></button>
                 </div>
-            </div>    
-    </nav>
+            </div>
+            <div class="col-sm-10">
+               <div class="collapse navbar-collapse" id="navi">   
+                <ul class="nav navbar-nav">
+                    <li><a href="index.php">Home</a></li>
+                    <li><a href="menu.php">Menu</a></li>
+                    <li><a href="order.php">Order</a></li>
+                    <li><a href="about.php">About</a></li>
+
+                </ul>
+                <ul class="nav navbar-nav navbar-right">
+                    <li><a href="register.php"><span class="glyphicon glyphicon-user"> </span> Sign Up</a></li>
+                    <li><a href="login.php"><span class="glyphicon glyphicon-log-in"></span> Login</a></li>
+                </ul>    
+            </div>
+        </div>
+    </div>
+</div>    
+</nav>
     <!--NAV CLOSE-->
